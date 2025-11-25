cdfe8fc02313d3e4fe58fca71472769301ab67ec
Dat Le <ldat0909@gmail.com>
2025-11-26 00:09:18 +0700

    Fix api flow at routes

--- src/routes/chatbot.routes.ts
+++ src/routes/chatbot.routes.ts
@@ -4,5 +4,5 @@ import { handleChat } from "../controller/chatbot.controller";
 import { verifyUser } from "../middlewares/verifyUser";
 
 const router = express.Router();
-router.post("/", verifyUser, handleChat);
+router.post("/", handleChat);
 export default router;
